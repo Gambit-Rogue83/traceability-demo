fafb3613f7948985ccda230f6cf3843c7b757c8e
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2023-12-21 13:24:10 -0800

    rollbar added

--- server/server.js
+++ server/server.js
@@ -5,6 +5,16 @@ const path = require('path')
 app.use(express.json())
 app.use(cors())
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'bc62195e244f442bb1be3cb207ce5200',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use(express.static(path.join(__dirname, "../public")))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
